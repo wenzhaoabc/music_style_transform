f41a3b6c4ce857a7875ac3d75ab141bbabd09503
WEN <wenzhaoabc@gmail.com>
2023-03-19 15:41:38 +0800

    add: 乐器增删查

--- api/routes.py
+++ api/routes.py
@@ -6,6 +6,11 @@ import json
 
 import pymysql.cursors
 from flask import request, session
+# 设置当前路径
+import sys
+
+sys.path.insert(0, "D:\\WorkSpace\\Python\\style_transform\\music_style_transform\\")
+sys.path.append("/workspace")
 
 from api import create_app
 from dbconn import get_db
@@ -287,6 +292,199 @@ def flask_upload_files_by_form():
         msg = '存在未上传成功的文件'
     finally:
         result = dict(code=code, data=data, msg=msg)
+    return json.dumps(result)
+
+
+@app.route('/instrument/upload', methods=['POST'])
+def upload_single_instrument():
+    """
+    上传乐器，数据位于form中，需要字段为`name`,`description`,`category`,`image`,
+    前三个为文本类型，`image`为file类型
+
+    返回数据中data域形式为
+    {"id":"","name":"","image":"图片URL","description":"","category":""}
+    :return:
+    """
+    code = ResponseCode.success
+    msg = ''
+    data = None
+    name = ''
+    description = ''
+    category = ''
+    image_url = ''
+
+    try:
+        name = request.form['name']
+        description = request.form['description']
+        category = request.form['category']
+        image = request.files['image']
+        image_url = upload_file(image, image.filename)
+    except KeyError:
+        msg = '参数错误或缺失'
+        code = ResponseCode.param_error
+        pass
+    except AttributeError:
+        msg = '图片缺失'
+        code = ResponseCode.param_missing
+
+    if code is ResponseCode.success:
+        db_conn = get_db()
+        cursor = db_conn.cursor(pymysql.cursors.DictCursor)
+        try:
+            cursor.execute(
+                "INSERT INTO db_music_trans.t_instrument(`name`,`image`,`description`,`category`) VALUES (%s,%s,%s,%s)",
+                (name, image_url, description, category))
+            db_conn.commit()
+            cursor.execute("SELECT LAST_INSERT_ID() AS id")
+            insert_id = cursor.fetchone().get('id')
+            data = dict(id=insert_id, name=name, image=image_url, description=description, category=category)
+        except db_conn.Error:
+            db_conn.rollback()
+            code = ResponseCode.db_conn_error
+            msg = '连接数据库错误'
+            data = {}
+            pass
+        finally:
+            cursor.close()
+
+    msg = 'success'
+    result = dict(code=code, msg=msg, data=data)
+    return json.dumps(result)
+
+
+def get_single_instrument_by_id(id):
+    """
+    获取特定ID的乐器信息，GET请求，参数位于URL中 id=<>
+    :return: data域为乐器信息{id,name,description,image,category}
+    """
+    code = ResponseCode.success
+    msg = 'success'
+    data = {}
+
+    try:
+        id = int(id)
+    except Exception:
+        code = ResponseCode.param_error
+        msg = '参数错误'
+
+    if code is ResponseCode.success:
+        db_conn = get_db()
+        cursor = db_conn.cursor(pymysql.cursors.DictCursor)
+
+        try:
+            cursor.execute("SELECT * FROM db_music_trans.t_instrument WHERE id = %s", (id))
+            data = cursor.fetchone()
+            if data is None:
+                code = ResponseCode.db_not_found
+                msg = '不存在相应数据'
+        except db_conn.Error:
+            code = ResponseCode.db_conn_error
+            msg = '数据库连接错误'
+        finally:
+            cursor.close()
+
+    result = dict(code=code, msg=msg, data=data)
+    return json.dumps(result)
+
+
+def search_instruments_by_name(instrument_name=None, instrument_category=None):
+    """
+    按名字/类别查找相应乐器，GET请求，参数name位于URL中
+    :param instrument_category: 类别关键字
+    :param instrument_name: 名字关键字
+    :return: data域为所有匹配的乐器，数组形式
+    """
+    code = ResponseCode.success
+    msg = ''
+    data = []
+
+    if (instrument_name is not None) and (instrument_category is None):
+        exec_sql = "SELECT * FROM db_music_trans.t_instrument WHERE LOCATE(%s,`name`)>0"
+        query_args = (instrument_name)
+    elif instrument_category is not None and (instrument_name is None):
+        exec_sql = "SELECT * FROM db_music_trans.t_instrument WHERE LOCATE(%s,`category`)>0"
+        query_args = (instrument_category)
+    else:
+        exec_sql = "SELECT * FROM db_music_trans.t_instrument WHERE LOCATE(%s,`name`)>0 AND LOCATE(%s,`category`)>0"
+        query_args = (instrument_name, instrument_category)
+
+    db_conn = get_db()
+    cursor = db_conn.cursor(pymysql.cursors.DictCursor)
+    try:
+        cursor.execute(exec_sql, query_args)
+        data = cursor.fetchall()
+    except db_conn.Error:
+        code = ResponseCode.db_conn_error
+        msg = '数据库连接错误'
+    finally:
+        cursor.close()
+
+    msg = len(data)
+    result = dict(code=code, msg=msg, data=data)
+    return json.dumps(result)
+
+
+def get_all_instruments():
+    """
+    获取所有的乐器
+    :return: data域为乐器信息的数组
+    """
+    code = ResponseCode.success
+    msg = 'success'
+    data = []
+    db_conn = get_db()
+    cursor = db_conn.cursor(pymysql.cursors.DictCursor)
+
+    try:
+        cursor.execute("SELECT * FROM db_music_trans.t_instrument")
+        data = cursor.fetchall()
+    except db_conn.Error:
+        code = ResponseCode.db_conn_error
+        msg = '连接数据库错误'
+    finally:
+        cursor.close()
+
+    msg = len(data)
+    result = dict(code=code, msg=msg, data=data)
+    return json.dumps(result)
+
+
+@app.route('/instrument', methods=['GET'])
+def get_instruments_by_arges():
+    instrument_name = request.args.get('name')
+    instrument_id = request.args.get('id')
+    instrument_category = request.args.get('category')
+
+    if instrument_name or instrument_category:
+        return search_instruments_by_name(instrument_name, instrument_category)
+    elif instrument_id:
+        return get_single_instrument_by_id(instrument_id)
+    else:
+        return get_all_instruments()
+
+
+@app.route('/instrument', methods=['DELETE'])
+def delete_single_instrument():
+    instrument_id = request.args.get('id')
+    code = ResponseCode.success
+    msg = 'success'
+
+    if instrument_id is None:
+        code = ResponseCode.param_missing
+        msg = '缺失乐器ID参数'
+
+    db_conn = get_db()
+    cursor = db_conn.cursor(pymysql.cursors.DictCursor)
+    if ResponseCode.success is code:
+        try:
+            cursor.execute("DELETE FROM db_music_trans.t_instrument WHERE `id` = %s", (instrument_id))
+        except db_conn.Error:
+            code = ResponseCode.db_conn_error
+            msg = '数据库连接错误'
+        finally:
+            cursor.close()
+
+    result = dict(code=code, msg=msg)
     return json.dumps(result)
 
 
